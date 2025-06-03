8a4ff8be9d7a0b612a8b9d873b642f9004a9c6c7
Anthony G. <antgill101@live.com>
2025-06-03 15:56:28 -0600

    Created an initial outline and class for UVSim python file

---
+++ UVSim.py
@@ -0,0 +1,34 @@
+#Names:
+#Project:
+#Description:
+
+## IMPORTS
+import sys
+
+
+class UVSIM:
+    def __init__():
+        #Set up memory and accumulator
+        pass
+
+    def read_file(self, file):
+        #Opens and reads the file that is input on the Command Line
+        pass
+
+    def execute(self):
+        #Loops through the memory and performs the action for each opcode
+
+        """
+        My thoughts would be to have something like and if statement to find the correct
+        opcode and then just call the operation on it as its own function.
+        """
+        pass
+
+    #### I/O Operations ####
+
+    #### LOAD/STORE Operations ####
+
+    #### Arithmetic Operations ####
+
+    #### Control Operations ####
+
