865debaa00b9a5bf4b12169013f4af6aac458380
Andie Merino <ayndvee@gmail.com>
2025-06-16 20:52:50 -0600

    Unit Test Merge

--- UVSim-Project/tests/test_UVSim.py
+++ UVSim-Project/tests/test_UVSim.py
@@ -1,5 +1,7 @@
 import unittest
 from src.UVSim import UVSIM
+from unittest.mock import patch
+from io import StringIO
 
 ### HERE is how to run the tests PYTHONPATH=src python -m unittest discover -s tests ###
 
@@ -122,3 +124,32 @@ class TestUVSIM(unittest.TestCase):
         self.sim.divide(30)
         #Error message is printed, but we can't capture it in this test
         self.assertEqual(self.sim.accumulator, 500) 
+    
+     # --- LOAD / STORE Tests ---
+
+    def test_load(self):
+        # memory[10] = 1234, expect accumulator = 1234 after load
+        self.sim.memory[10] = 1234
+        self.sim.load(10)
+        self.assertEqual(self.sim.accumulator, 1234)
+
+    def test_store(self):
+        # accumulator = -5678, expect memory[20] = -5678 after store
+        self.sim.accumulator = -5678
+        self.sim.store(20)
+        self.assertEqual(self.sim.memory[20], -5678)
+
+    # --- I/O Tests ---
+
+    @patch('builtins.input', return_value='+3456')
+    def test_read(self, mock_input):
+        # Simulates user entering "3456" as input
+        self.sim.read(5)
+        self.assertEqual(self.sim.memory[5], 3456)
+
+    @patch('sys.stdout', new_callable=StringIO)
+    def test_write(self, mock_stdout):
+        # memory[7] = -2222, expect output "-2222\n"
+        self.sim.memory[7] = -2222
+        self.sim.write(7)
+        self.assertEqual(mock_stdout.getvalue(), "-2222\n")
